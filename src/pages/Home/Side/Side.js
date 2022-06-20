043931023448719a590e929e866686a99ac5e6b8
firoj7github <fkfiroj02@gmail.com>
2022-06-20 02:56:54 -0700

    clip add

--- src/pages/Home/Side/Side.js
+++ src/pages/Home/Side/Side.js
@@ -8,7 +8,12 @@ const Side = () => {
     return (
         <div className='side-all'>
           <div className='side-clip'>
-
+          <h3>Grow Your Personality</h3>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.
+          <br/>
+          <br/>
+          
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab.</p>
           </div>
           <div className='side-img'>
           <img src={icon} alt="Gym"></img>
